7ba6895d48cd60fd877e8fc362448e559c8223cb
cl-rs <tucater10896601@protonmail.com>
2021-07-10 09:59:07 +0530

    gamma categorization.txt added

---
+++ Design_Patterns_In_Modern_C++_Reusable_Approaches_For_Object-Oriented_Software_Design/gamma_categorization.cpp
@@ -0,0 +1,21 @@
+Gamma Categorization
+
+* Design patterns are typically split into three categories
+* This is called Gamma categorization after Erich Gamma, one of GOF authors
+* Creational Patterns
+* 	Deal with creation (construction) of objects
+* 	Explicit(consturctor) vs implicit (DI, reflection, etc.)
+	DI reflection etc is implicit object creation (DI= dependency injection)
+	Wholsale (single statement ) vs piecewise (step-by-step)
+	Several steps to be taken before object is need to be used
+
+
+*Structural Patterns
+	* Concerned with the structure (e.g., class members)
+	* Many patterns are wrappers that mimic the underlying class interface
+	* Stress the importance of good API desing
+	*
+* Behavioural Patterns
+* 	They don't really follow any central theme.
+* 	They solve it in a particualr way
+*

01d4f70aba01305911673293e5ecae094b280e85
cl-rs <tucater10896601@protonmail.com>
2021-07-21 22:40:22 +0530

    iterators_code(incomplete) added

---
+++ Design_Patterns_In_Modern_C++_Reusable_Approaches_For_Object-Oriented_Software_Design/iterator.cpp
@@ -0,0 +1,12 @@
+/*facilitates traversal of data structures
+ * Iterator is a core functionality of various data structures
+ * An iterator is a class that facilitates the traversal
+ 	* keeps a reference to the current element
+	* Knows how to move to a different element
+	*
+	*
+/
+
+
+
+

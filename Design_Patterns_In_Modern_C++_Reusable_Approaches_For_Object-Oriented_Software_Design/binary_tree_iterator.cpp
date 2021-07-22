b106d6df84268f2482619f9a1ec759eada216d33
cl-rs <tucater10896601@protonmail.com>
2021-07-22 12:25:49 +0530

    binary_tree_iterator_full_code_added

--- Design_Patterns_In_Modern_C++_Reusable_Approaches_For_Object-Oriented_Software_Design/binary_tree_iterator.cpp
+++ Design_Patterns_In_Modern_C++_Reusable_Approaches_For_Object-Oriented_Software_Design/binary_tree_iterator.cpp
@@ -54,7 +54,78 @@ template<typename T> struct BinaryTree
 		PreOrderIterator(Node<U> *current) :current(current){}
 		bool operator != (const PreOrderIterator<U> & other)
 		{
+			return current != other.current;
+		}
+
+		PreOrderIterator<U> & operator ++()
+		{
+			if(current->right)
+			{
+				current=current->right;
+				while(current->left)
+					current=current->left;
+			}
+
+
+			else
+			{
+				Node<T> * p= current->parent;
+				while ( p && current == p->right)
+				{
+					current=p;
+					p=p->parent;
+				}
+
+				current=p;
+			}
+
+			return *this;
+		}
+		Node<U> & operator * () {return *current;}
+	};
+
+	typedef PreOrderIterator<T> iterator;
+	iterator begin()
+	{
+		Node<T> * n= root;
+		if(n)
+			while(n->left)
+				n=n->left;
+		return iterator {n};
+	}
+
+	iterator end()
+	{
+		return iterator {nullptr};
+	}
 };
 
 
+int main()
+{
+	//     me
+	//     / \
+	//
+	// mother father
+	// /   \
+	// m'm  m'f
+	
+	//let's build the given tree
+	BinaryTree<std::string> family
+	{
+		new Node<std::string> {"me",
+					new Node<std::string> {"mother",
+								new Node<std::string> {"mother's mother"},
+								new Node<std::string> {"mother's father"},
+					},
+					new Node<std::string> {"father"}
+				}
+	};
+	for(auto it  =family.begin(); it!=family.end(); ++it)
+	{
+		std::cout<<(*it).value<<std::endl;
+	}
 
+	return 0;
+
+}

0a55b7e1038736f344bfa373e334f5200f70ade4
cl-rs <tucater10896601@protonmail.com>
2021-07-23 22:09:25 +0530

    neural_network_architecture almost complete code added

--- Design_Patterns_In_Modern_C++_Reusable_Approaches_For_Object-Oriented_Software_Design/neural_networks_architecture.cpp
+++ Design_Patterns_In_Modern_C++_Reusable_Approaches_For_Object-Oriented_Software_Design/neural_networks_architecture.cpp
@@ -1,53 +1,108 @@
-
-/*
- * software constructs taht attempt to mimic the way neuronos work in our brains
-*/
-#include<vector>
 #include<iostream>
+#include<ostream>
+#include<vector>
+#include<string>
 
-struct Neuron
+template<typename Self> struct SomeNeurons;
+struct Neuron: SomeNeurons<Neuron>;
+//struct Neuron;
+// we are going to use a CRTP
+// curiously recurring Template pattern
+
+template<typename Self>
+struct SomeNeurons
 {
-	std::vector<Neuron *> in, out;
-	unsigned int id;
-
-
-	Neuron()
+	template<typename T> void connect_to( T& other)
 	{
-		static int id=1;
-		this->id=id++;
-	}
-
-};;
-
-
-
-
-
-template<typename T, typename T1>
-
-
-{
-	out.push_back(&other);
-	other.in.push_back(this);
-}
-
-
-struct NeuronLayer:std::vector<Neuron>
-
-{
-	NeuronLayer(int count)
-	{
-		while(count -->0)
+		for (Neuron &from : *static_cast<Self*>(this))
 		{
-			emplace_back(Neuron{});
+			for(Neuron & to:other)
+			{
+				from.out.push_back(&to);
+				to.in.push_back(&from);
+			}
 		}
 	}
 };
+struct Neuron : SomeNeurons<Neuron>
+{
+	std::vector<Neuron*> in, out;
+	// for incoming and outgoing neuron;
+	//
+	//let's have a neuron id.
+	//we want our neuron to act as a collection that returns to itself.
+	//because that's the only element
+	//it's a scalar element
+	
+	unsigned int id;
+
+	Neuron()
+	{
+		static int id{1};
+		this->id = id++;
+	}
+
+	Neuron * begin() {return this;}
+	Neuron * end() { return this+1;}
 
 
+	friend std::ostream & operator<<(std::ostream & os, const Neuron &obj)
+	{
+		for(Neuron * n : obj.in)
+		{
+			os<<n->id<< " \t--->\t " << obj.id<<std::endl;
+		}
+
+		for( Neuron * n: obj.out)
+		{
+			os<< "[" << obj.id<< "]\t-->\t" << n-> id << std::endl;
+		}
+
+
+		return os;
+	}
+
+};
+
+
+//let's decide to make a neuron layer... 
+//A neuron layer is basically like a collection 
+// Instead of making one neuron, we make a pack of collection of neurons.
+// we could keep it in something like a vector.
+// typically we are not supposed to inherit from stl types as they don't have
+// virtual destructors and they might run into trouble.
+// but in this case we can do that.
+
+struct NeuronLayer: std::vector<Neuron>, SomeNeurons<NeuronLayer>
+{
+	NeuronLayer( int count)
+	{
+		while(count -- >0)
+			emplace_back(Neuron{});
+	}
+
+	friend std::ostream & operator <<(std::ostream & os, const NeuronLayer & obj)
+	{
+		for(auto &n: obj) os<<n;
+		return os;
+	}
+	// essentially we want to connect single layer to another layer.
+	//
+
+};
 int main()
 {
 	Neuron n1,n2;
+        n1.connect_to(n2);
+	std::cout<<n1<<n2<<std::endl;
+	// now we might want to connect a neuron to a layer
+	NeuronLayer layer1{2}, layer2{3};
+	n1.connect_to(layer1);
+	// now we might want to connect a layer to neuron
+	layer2.connect_to(n2);
+	// now we might also want to connect a layer to layer
+	layer1.connect_to(layer2);
+	// we want to do this using a common functionality using a single interface
 	return 0;
 }
 
